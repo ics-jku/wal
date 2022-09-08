6a83cdf1a7d03f17d520b87b0af81f631ec316a8
Lucas Klemmer <lucas.klemmer@jku.at>
2022-09-08 17:24:36 +0200

    wal style printing of arrays and quotes

--- wal/util.py
+++ wal/util.py
@@ -4,8 +4,12 @@ from wal.ast_defs import Symbol, Operator
 def wal_str(sexpr):
     '''Returns a string representation of a WAL expression'''
     if isinstance(sexpr, list):
-        if len(sexpr) == 3 and sexpr[0] == Operator.REL_EVAL:
+        if len(sexpr) == 2 and sexpr[0] == Operator.QUOTE:
+            txt = f"'{wal_str(sexpr[1])}"
+        elif len(sexpr) == 3 and sexpr[0] == Operator.REL_EVAL:
             txt = f'{sexpr[1].name}@sexpr[2]'
+        elif len(sexpr) > 0 and sexpr[0] == Operator.ARRAY:
+            txt = '{' + ' '.join(map(wal_str, sexpr)) + '}'
         else:
             txt = '(' + ' '.join(map(wal_str, sexpr)) + ')'
     elif isinstance(sexpr, Symbol):
